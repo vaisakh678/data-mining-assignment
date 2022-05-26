c7dba09f204b4c9cf0072ec437a8edcc6e54f0ec
vaisakh678 <vaisakh678@gmail.com>
2022-05-27 00:56:54 +0530

    oyi

--- Finder.py
+++ Finder.py
@@ -1,6 +1,7 @@
-def find(_x, _y, offset):
+def find(_x, _y, offset=0):
     for _ in range(offset):
-        print(' ', end="")
+        # print(' ', end="")
+        print(end=" ")
     for x, y in zip(_x, _y):
         if (x != y):
             print("^", end=" ")
